51dee9ad5a63b1de421ac62567bb62f81e1a5600
platipus25 <40722163+platipus25@users.noreply.github.com>
2020-01-21 12:09:10 -0800

    Refactor morning

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 
 int main (int argc, char* argv[]) {
 	time_t now;
-	struct tm morning;
+	struct tm* morning;
 	bool loop = false;
 	char* input = (char*)malloc(10 * sizeof(char));;
 
@@ -33,26 +33,26 @@ int main (int argc, char* argv[]) {
 
 	time(&now);  /* get current time; same as: timer = time(NULL)  */
 	
-	morning = *localtime (&now);
-	morning.tm_sec = 0;
+	morning = localtime(&now);
+	morning->tm_sec = 0;
 
-	parse(&morning, input);
+	parse(morning, input);
 
-	if(mktime(&morning) < now){
-		morning.tm_mday++;
+	if(mktime(morning) < now){
+		morning->tm_mday++;
 	}
 
 	if(loop){
-		printf("%s", asctime(&morning));
+		printf("%s", asctime(morning));
 	}
 	while(true) {
 		time(&now);
-		char* formatted = format(mktime(&morning), now);
+		char* formatted = format(mktime(morning), now);
 
 		printf("%s", formatted);
 
 		if(!loop){
-			printf(" until %s", asctime(&morning));
+			printf(" until %s", asctime(morning));
 			break;
 		}
 
